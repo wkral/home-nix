9672fe6686e3157943aaafcf756da2a7b32ef8b7
William Kral <william.kral@gmail.com>
2020-03-03 12:34:13 -0800

    Volume display on waybar update, wofi styling

--- modules/wofi/default.nix
+++ modules/wofi/default.nix
@@ -3,10 +3,12 @@
   home.packages = [
     pkgs.wofi
     pkgs.hicolor-icon-theme
+    pkgs.gnome3.adwaita-icon-theme
   ];
   xdg.configFile."wofi/config".text = ''
     width=600
     lines=7
+    allow_images=true
   '';
   xdg.configFile."wofi/style.css".source = ./style.css;
 }
