a59be007e5d24aca985d5589c7415c4295329bf6
William Kral <william.kral@gmail.com>
2023-03-09 15:29:42 -0800

    Allow SSH access from work-vm to host

--- hosts/livingroom/configuration.nix
+++ hosts/livingroom/configuration.nix
@@ -91,7 +91,10 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -182,6 +185,7 @@
       ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBSsYbuCFzk+qWYhj1C+bPkcfUKQQw9J51bt6FszVZaD"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILU/4DYvPYGqa8/jmoDo8rC1Yn3uEVfbTwXPhzZh8ZHX"
       ];
     };
     users.greeter.group = "greeter";
