877fd3eafcf3f4771bc455a27d20001064a5a01a
William Kral <william.kral@gmail.com>
2023-07-31 17:25:48 -0700

    Abstract host definition commonalities

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,55 +1,39 @@
-{ nixpkgs, sops-nix, home-manager, jovian,  ... }:
+{ nixpkgs, sops-nix, home-manager, jovian, ... }:
+let
+  host = { name, system, extraModules ? [ ] }: (nixpkgs.lib.nixosSystem {
+    inherit system;
+    modules = [
+      nixpkgs.nixosModules.notDetected
+      sops-nix.nixosModules.sops
+      ./common.nix
+      ./${name}/configuration.nix
+      ./${name}/hardware-configuration.nix
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.wkral = import ./${name}/wkral.nix;
+      }
+    ] ++ extraModules;
+  });
+in
 {
-  livingroom = nixpkgs.lib.nixosSystem {
+  livingroom = host {
+    name = "livingroom";
     system = "x86_64-linux";
-    modules = [
-      nixpkgs.nixosModules.notDetected
-      sops-nix.nixosModules.sops
-      ./common.nix
-      ./common-gui.nix
-      ./livingroom/configuration.nix
-      ./livingroom/hardware-configuration.nix
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.wkral = import ./livingroom/wkral.nix;
-      }
-    ];
+    extraModules = [ ./common-gui.nix ];
   };
-  deck = nixpkgs.lib.nixosSystem {
+  deck = host {
+    name = "deck";
     system = "x86_64-linux";
-    modules = [
-      nixpkgs.nixosModules.notDetected
-      sops-nix.nixosModules.sops
-      ./common.nix
+    extraModules = [
       ./common-gui.nix
-      ./deck/configuration.nix
-      ./deck/hardware-configuration.nix
       (jovian + "/modules")
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.wkral = import ./deck/wkral.nix;
-      }
     ];
   };
-  work-vm = nixpkgs.lib.nixosSystem {
+  work-vm = host {
+    name = "work-vm";
     system = "x86_64-linux";
-    modules = [
-      nixpkgs.nixosModules.notDetected
-      sops-nix.nixosModules.sops
-      ./common.nix
-      ./common-gui.nix
-      ./work-vm/configuration.nix
-      ./work-vm/hardware-configuration.nix
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.wkral = import ./work-vm/wkral.nix;
-      }
-    ];
+    extraModules = [ ./common-gui.nix ];
   };
 }
