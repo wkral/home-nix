687cdd4243be51fa29eb715a8f30576e30f37a14
William Kral <william.kral@gmail.com>
2023-08-01 02:27:21 -0700

    Enable sway idle on deck

--- hosts/deck/wkral.nix
+++ hosts/deck/wkral.nix
@@ -9,6 +9,11 @@
   wk = {
     gui = {
       enable = true;
+      idle = {
+        enable = true;
+        screen-poweroff = 1200;
+        lock = 3600;
+      };
       font.base-size = 14;
       outputs.primary = "eDP-1";
       app-indicators = {
