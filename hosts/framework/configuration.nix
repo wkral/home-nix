7845f6de6be44f31d771de57266fd895b0749153
William Kral <william.kral@gmail.com>
2023-10-24 15:52:51 -0700

    Add regreet to framework

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -7,6 +7,31 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  programs.regreet =
+    let
+      background = pkgs.fetchurl
+        {
+          url = "https://w.wallhaven.cc/full/4o/wallhaven-4owxz7.jpg";
+          sha256 = "1cga9vzwbaa63xcfh8i6j8c88rrlxv5gv5awl3409swfhqfd6c3b";
+        };
+    in
+    {
+      enable = true;
+      settings = {
+        background = {
+          path = background;
+          fit = "Cover";
+        };
+        GTK = {
+          font_name = "Noto Sans 18";
+          theme_name = "Dracula";
+          icon_theme_name = "dracula";
+          cursor_theme_name = "Bivata-Modern-Ice";
+          application_prefer_dark_theme = true;
+        };
+      };
+    };
+
   networking = {
     hostName = "framework"; # Define your hostname.
     networkmanager.enable = true;
