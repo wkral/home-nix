c76bef485288e541ae5cf8ef56d3f03f580a9335
William Kral <william.kral@gmail.com>
2024-02-15 12:35:01 -0800

    Update inputs and adjust renamed options

--- hosts/deck/configuration.nix
+++ hosts/deck/configuration.nix
@@ -75,9 +75,9 @@ in
   };
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   hardware.bluetooth = {
