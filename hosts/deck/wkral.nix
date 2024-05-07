195cc9c9c937a03a394f6b798e3db8f5e194ad06
William Kral <william.kral@gmail.com>
2024-05-07 10:42:18 -0700

    deck: suspend directly after pipewire idle-inhibit

--- hosts/deck/wkral.nix
+++ hosts/deck/wkral.nix
@@ -11,13 +11,9 @@
   wk = {
     gui = {
       enable = true;
-      idle.screen-off = {
-        enable = true;
-        timeout = 300;
-      };
       idle.suspend = {
         enable = true;
-        timeout = 5400;
+        timeout = 600;
       };
       backlight-control.enable = true;
       font.base-size = 14;
