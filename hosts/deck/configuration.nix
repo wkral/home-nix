de1436c6c879784df75a0a44aa8758f76b34c96a
William Kral <william.kral@gmail.com>
2024-09-19 09:42:23 -0700

    deck: Add jovian overlay to nixpkgs

--- hosts/deck/configuration.nix
+++ hosts/deck/configuration.nix
@@ -2,11 +2,15 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 let ids = import ../ids.nix;
 in
 {
+  nixpkgs.overlays = [
+    inputs.jovian.overlays.default
+  ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
