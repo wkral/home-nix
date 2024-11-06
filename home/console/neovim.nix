e020d09878063164b18cd414b93c04bd8434d424
William Kral <william.kral@gmail.com>
2024-11-06 10:34:01 -0800

    neovim: Use pyright lsp when available

--- home/console/neovim.nix
+++ home/console/neovim.nix
@@ -75,7 +75,7 @@
 
           -- Use a loop to conveniently call 'setup' on multiple servers and
           -- map buffer local keybindings when the language server attaches
-          local servers = { 'rust_analyzer', 'nixd', 'gopls', 'golangci_lint_ls' }
+          local servers = { 'rust_analyzer', 'nixd', 'gopls', 'golangci_lint_ls', 'pyright' }
           for _, lsp in pairs(servers) do
             require('lspconfig')[lsp].setup {
               on_attach = on_attach,
