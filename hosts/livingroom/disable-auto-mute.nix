a595438abe1e3b941407279fa2e336b0a96631a8
William Kral <william.kral@gmail.com>
2025-03-17 13:12:24 -0700

    niri: Fix auto-mute-disable to be part of niri service

--- hosts/livingroom/disable-auto-mute.nix
+++ hosts/livingroom/disable-auto-mute.nix
@@ -3,11 +3,11 @@
   systemd.user.services.disable-auto-mute = {
     Unit = {
       Description = "Disable auto-mute so speakers can work with headphones plugged in";
-      After = [ "sway-session-pre.target" "pipewire.service" ];
-      PartOf = [ "sway-session.target" "pipewire.service" ];
+      After = [ "niri.service" "pipewire.service" ];
+      PartOf = [ "niri.service" "pipewire.service" ];
     };
 
-    Install = { WantedBy = [ "sway-session.target" ]; };
+    Install = { WantedBy = [ "niri.service" ]; };
     Service = {
       ExecStart = "${pkgs.alsa-utils}/bin/amixer -c Generic sset 'Auto-Mute Mode' Disabled";
       Type = "oneshot";
