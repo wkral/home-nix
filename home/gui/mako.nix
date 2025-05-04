25b64301a980dec99aaebf32a734eb77737c98e0
William Kral <william.kral@gmail.com>
2025-05-04 09:14:35 -0700

    all: update inputs, fix changed options

--- home/gui/mako.nix
+++ home/gui/mako.nix
@@ -5,13 +5,32 @@ in
 {
   services.mako = {
     enable = true;
-    anchor = "bottom-right";
-    font = "Noto Sans Light " + font-base;
-    defaultTimeout = 10000;
-    backgroundColor = "#1d1f21e6";
-    borderColor = "#3d3f41e6";
-    borderRadius = 5;
-    textColor = "#eeeeeeff";
+    settings = {
+      anchor = "bottom-right";
+      font = "Noto Sans Light " + font-base;
+      default-timeout = "10000";
+      background-color = "#1d1f21e6";
+      border-color = "#3d3f41e6";
+      border-radius = "5";
+      border-size = "1";
+      text-color = "#eeeeeeff";
+      width = "300";
+      height = "100";
+      margin = "10";
+      padding = "5";
+      max-visible = "5";
+      max-history = "5";
+      sort = "-time";
+      layer = "top";
+      ignore-timeout = "false";
+      format = "<b>%s</b>\n%b";
+      actions = "true";
+      markup = "true";
+      progress-color = "over #5588AAFF";
+      icons = "true";
+      max-icon-size = "64";
+
+    };
   };
 
   systemd.user.services.mako = {
@@ -22,10 +41,12 @@ in
       ConditionEnvironment = "WAYLAND_DISPLAY";
     };
 
-    Install = { WantedBy = [ "niri.service" ]; };
+    Install = {
+      WantedBy = [ "niri.service" ];
+    };
 
     Service = {
-      ExecStart = "${pkgs.mako}/bin//mako";
+      ExecStart = "${pkgs.mako}/bin/mako";
       Type = "dbus";
       BusName = "org.freedesktop.Notifications";
       ExecReload = "${pkgs.mako}/bin/makoctl reload";
